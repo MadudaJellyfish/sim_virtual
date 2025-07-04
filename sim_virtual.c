1915ba8946c102025e5ca34f49b23b43ed64bd7a
maria eduarda <mariaeduardalivros@gmail.com>
2025-07-04 19:22:57 -0300

    implementação do LRU

--- sim_virtual.c
+++ sim_virtual.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 struct sim_virtual
 {
@@ -8,9 +9,104 @@ struct sim_virtual
     int pag_referenciada; // 1 se a página foi referenciada, 0 caso contrário
     int pag_modificada;   // 1 se a página foi modificada, 0
     int ultimo_acesso; // último acesso à página
+    int em_uso;
 };
 
+struct param_sim
+{
+    int qtd_pag_mem;
+    char* nome_arquivo_acessos;
+    int tam_page;
+};
+
+typedef struct param_sim Param_Sim;
+
 typedef struct sim_virtual SimVirtual;
+
+void lru_algoritmo(Param_Sim param, SimVirtual* buffer_mem_fisica, int* qtd_pgs_sujas, int* qtd_pgs_faults)
+{
+    FILE* arq_acessos;
+    if(!(arq_acessos = fopen(param.nome_arquivo_acessos, "r")))
+    {
+        printf("Nao foi possivel abrir o arquivo %s!!!\n", param.nome_arquivo_acessos);
+        exit(1);
+    }
+
+    unsigned addr;
+    char r_or_w;
+
+    unsigned shift_page = log2(param.tam_page*1024);
+    int time = 0;
+    
+    while (fscanf(arq_acessos, "%x %c ", &addr, &r_or_w) == 2)
+    {
+        int page = addr>>shift_page;
+        int is_page_in_mem = 0; //0 se não estiver, 1 c.c.
+        for (int i = 0; i < param.qtd_pag_mem; i++)
+        {
+            //Se a página estiver na memória
+            if(page == buffer_mem_fisica[i].endereco_fisico)
+            {
+                if(r_or_w=='W') buffer_mem_fisica[i].pag_modificada = 1;
+                else buffer_mem_fisica[i].pag_referenciada = 1;
+                buffer_mem_fisica[i].ultimo_acesso = time;
+                is_page_in_mem = 1;
+                break;
+            }
+        }
+
+        int was_page_inserted = 0;
+        //caso a página não esteja na memória é gerado um page fault
+        if(is_page_in_mem==0)
+        {
+            for (int i = 0; i < param.qtd_pag_mem; i++)
+            {
+                if(buffer_mem_fisica[i].em_uso == 0)
+                {
+                    buffer_mem_fisica[i].endereco_fisico = page;
+                    buffer_mem_fisica[i].pag_modificada = 0;
+                    buffer_mem_fisica[i].pag_referenciada = 0;
+                    buffer_mem_fisica[i].ultimo_acesso = time;
+                    buffer_mem_fisica[i].em_uso = 1;
+
+                    was_page_inserted = 1;
+                    break;
+                }
+            }
+            
+
+            //alguma página precisa ser removida, neste caso, a com o acesso mais antigo
+            if (was_page_inserted==0)
+            {
+                int uso_mais_antigo = 100000000000000000000;
+                int i_older_page = 0;
+                for (int i = 0; i < param.qtd_pag_mem; i++)
+                {
+                    if(buffer_mem_fisica[i].ultimo_acesso < uso_mais_antigo)
+                    {
+                        uso_mais_antigo = buffer_mem_fisica[i].ultimo_acesso;
+                        i_older_page = i;
+                    } 
+                }
+
+                if(buffer_mem_fisica[i_older_page].pag_modificada == 1) *qtd_pgs_sujas++;
+
+                buffer_mem_fisica[i_older_page].endereco_fisico = page;
+                buffer_mem_fisica[i_older_page].pag_modificada = 0;
+                buffer_mem_fisica[i_older_page].pag_referenciada = 0;
+                buffer_mem_fisica[i_older_page].ultimo_acesso = time;
+
+            }
+            
+            *qtd_pgs_faults++;
+        }
+
+        time++;
+        
+    }
+    
+    
+}
 
 int main(int argc, char* argv[])
 {
@@ -20,11 +116,11 @@ int main(int argc, char* argv[])
     }
     //serão recebidos ao total 4 argumentos na chamada do programa
     //o primeiro é o tipo od algoritmo a ser utilizado
-    char* tipo_algoritmo;
+    char tipo_algoritmo[50];
     strcpy(tipo_algoritmo, argv[1]);
 
     //o segundo é o nome do arquivo com os acessos à memória
-    char* nome_arquivo_acessos;
+    char nome_arquivo_acessos[50];
     strcpy(nome_arquivo_acessos, argv[2]);
 
     //o terceiro é o tamanho de cada página
@@ -33,15 +129,15 @@ int main(int argc, char* argv[])
     //o quarto é o tamanho da memória física
     unsigned tam_memoria_fisica = atoi(argv[4]);
 
-    if(tam_pag_mem != 8 || tam_pag_mem != 32)
+    if(tam_pag_mem != 8 && tam_pag_mem != 32)
     {
-        printf("Tamanho de páginas inválido!!!\n");
+        printf("Tamanho de páginas inválido!!! Ou 8KB ou 32 KB\n");
         exit(1);
     }
 
-    if(tam_memoria_fisica != 1 || tam_memoria_fisica != 2)
+    if(tam_memoria_fisica != 1 && tam_memoria_fisica != 2)
     {
-        printf("Tamanho de páginas inválido!!!\n");
+        printf("Tamanho de memória inválido!!! Ou 1MB ou 2MB\n");
         exit(1);
     }
 
@@ -50,19 +146,41 @@ int main(int argc, char* argv[])
     qtd_pag_mem = (tam_memoria_fisica*1024) / tam_pag_mem;   
 
     //alocação do vetor de páginas físicas
-    SimVirtual* buffer_mem_fisica = (SimVirtual*)malloc(qtd_pag_mem * sizeof(SimVirtual));
-    if(!buffer_mem_fisica) {
-        perror("Erro ao alocar memória para paginas físicas");
-        exit(1);
+    SimVirtual buffer_mem_fisica[qtd_pag_mem]; 
+    memset(buffer_mem_fisica, 0, sizeof(buffer_mem_fisica));
+
+    int qtd_pgs_sujas = 0;
+    int qtd_pgs_faults = 0;
+
+    Param_Sim params;
+    strcpy(params.nome_arquivo_acessos, nome_arquivo_acessos);
+    params.qtd_pag_mem = qtd_pag_mem;
+    params.tam_page = tam_pag_mem;
+    
+    printf("[Executando simulador...]\n");
+    printf("Arquivo de entrada: %s\n", nome_arquivo_acessos);
+    printf("Tamanho memoria fisica: %d MB\n", tam_memoria_fisica);
+    printf("Tamanho das paginas: %d KB\n", tam_pag_mem);
+    printf("Algoritmo de Substituicao: %s\n", tipo_algoritmo);
+    if(strcmp(tipo_algoritmo, "LRU")==0)
+    {
+        lru_algoritmo(params, buffer_mem_fisica, &qtd_pgs_sujas, &qtd_pgs_faults);
     }
 
-    for (int i = 0; i < qtd_pag_mem; i++)
+    else if(strcmp(tipo_algoritmo, "2nd chance")==0)
     {
-        buffer_mem_fisica[i].endereco_fisico = 0; // inicializa o endereço físico da página
-        buffer_mem_fisica[i].pag_referenciada = 0; // inicializa a página como não referenciada
-        buffer_mem_fisica[i].pag_modificada = 0; // inicializa a página como não modificada
-        buffer_mem_fisica[i].ultimo_acesso = 0; // inicializa o último
+        second_chance_algoritmo(params, buffer_mem_fisica, &qtd_pgs_sujas, &qtd_pgs_faults);
     }
-    
+    else if(strcmp(tipo_algoritmo, "clock")==0)
+    {
+        clock_algoritmo(params, buffer_mem_fisica, &qtd_pgs_sujas, &qtd_pgs_faults);
+    }
+    else //algoritmo ótimo
+    {
+        algoritmo_otimo(params, buffer_mem_fisica, &qtd_pgs_sujas, &qtd_pgs_faults);
+    }
+
+    printf("Numero de faltas de páginas: %d\n", qtd_pgs_faults);
+    printf("Numero de páginas escritas: %d\n", qtd_pgs_sujas);
 
 }
